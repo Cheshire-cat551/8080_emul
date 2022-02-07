0dcd1c27a6cebd6f85930f34ccbb637583daf682
Cheshire-cat551 <85074324+Cheshire-cat551@users.noreply.github.com>
2022-02-07 15:14:47 +0530

    Add files via upload

---
+++ 8080_emul.c
@@ -0,0 +1,15 @@
+#include<stdc++.h>
+#include<stdlib.h>
+#include<string.h>
+#include<stdint.h>
+
+unsigned char A,B,C,D,E; // registers in 8080
+unsigned char* pc;       // program counter
+ 
+// each instruction in 8080 is 1-3 bytes
+// timing important factor
+int main(void){
+
+	return 0;
+}
+
