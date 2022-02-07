4f2bd83fd0c4664dcc76f9bfd830489c2905265a
Cheshire-cat551 <85074324+Cheshire-cat551@users.noreply.github.com>
2022-02-07 15:15:09 +0530

    Add files via upload

--- 8080_emul.c
+++ 8080_emul.c
@@ -3,8 +3,24 @@
 #include<string.h>
 #include<stdint.h>
 
-unsigned char A,B,C,D,E; // registers in 8080
-unsigned char* pc;       // program counter
+typedef struct ConditionCodes{
+	uint8_t 
+}
+
+// Stores state of all 8080 registers 
+typedef struct State{
+	uint8_t a;
+	uint8_t b;
+	uint8_t c;
+	uint8_t d;
+	uint8_t e;
+	uint8_t h;
+	uint8_t l;
+	uint8_t sp;
+	uint8_t pc;
+	uint8_t *mem;
+}State;
+
  
 // each instruction in 8080 is 1-3 bytes
 // timing important factor
